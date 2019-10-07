2f179be139cec08e88b1e56a60fa4dd0445267f9
Ramzibawayza <ramzibawayza12@gmail.com>
2019-10-07 15:31:07 +0300

    edit

---
+++ index1.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var love = prompt('Is the last name of \"Ramzi\" is \"Bawayza\"?')
+love = love.toLowerCase();
+
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+    alert('Great answer');
+} else if (love == 'y') {
+    alert('Great answer');
+} else if (love == 'no') {
+    alert('Oooh, wrong annswer');
+} else if (love == 'n') {
+    alert('Oooh, wrong annswer');
+}
+else {
+    alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+
+var love = prompt('Is Ramzi age 28 years?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+    alert('WHAT DID YOU SAY!, Do you think I am at the age of your father, Certainly wrong answer.');
+} else if (love == 'y') {
+    alert('WHAT DID YOU SAY!, Do you think I am at the age of your father, Certainly wrong answer.');
+} else if (love == 'no') {
+    alert('Great one, keep going');
+} else if (love == 'n') {
+    alert('Great one, keep going');
+}
+else {
+    alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+
+var love = prompt('Has Ramzi studied at the University of Jordan?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+    alert('Oh my God, Do you think i am of the bourgeois class?');
+} else if (love == 'y') {
+    alert('Oh my God, Do you think i am of the bourgeois class?');
+} else if (love == 'no') {
+    alert('Wow, Great answer');
+} else if (love == 'n') {
+    alert('Wow, Great answer');
+}
+else {
+    alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+
+var love = prompt('Is Ramzi favorite team is Real Madrid?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+    alert('Of course it is');
+} else if (love == 'y') {
+    alert('Of course it is');
+} else if (love == 'no') {
+    alert('Oooh, wrong annswer, sorry about that!');
+} else if (love == 'n') {
+    alert('Oooh, wrong annswer, sorry about that!');
+}
+else {
+    alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+
+var love = prompt('do Ramzi love coding?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+    alert('Of course i do');
+} else if (love == 'y') {
+    alert('Of course i do');
+} else if (love == 'no') {
+    alert('Sorry about that, wrong annswer');
+} else if (love == 'n') {
+    alert('Sorry about that, wrong annswer');
+}
+else {
+    alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
